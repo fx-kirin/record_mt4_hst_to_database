d8b5fa879bfdb827e9946dcbd8917e20208292c2
Yoshiaki Ono <ono.kirin@gmail.com>
2018-06-01 17:56:51 +0900

    fix memory leak

--- record_hst_to_mysql.py
+++ record_hst_to_mysql.py
@@ -62,11 +62,14 @@ def record_hst_data_to_mysql(hst_data_directory, database_yaml_path):
     session = Session()
     conn = engine.connect()
     
-    metadata = MetaData()
-    metadata.reflect(engine)
-    
     tables = conn.execute('SHOW TABLES').fetchall()
     tables = [table_name for table_name, in tables]
+    
+    alchemy_tables = Base.classes.keys()
+    for table in tables:
+        if table not in alchemy_tables:
+            conn.execute('DROP TABLE %s;'%(table))
+            
     
     hst_wild_card = os.path.join(hst_data_directory, "*.hst")
     hst_files = glob.glob(hst_wild_card)
@@ -74,17 +77,31 @@ def record_hst_data_to_mysql(hst_data_directory, database_yaml_path):
         name = re.sub("\..+$", "", os.path.basename(hst)).lower()
         df = tickdata(hst)
         df = df[:-1]
-        if name not in tables:
+        if name not in alchemy_tables:
             df.index += 1 
-            df.to_sql(name, conn, index_label='id')
-            conn.execute('ALTER TABLE `%s` ADD PRIMARY KEY (`id`);'%(name))
-            conn.execute('ALTER TABLE `%s` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;'%(name))
+            create_statement = """
+                CREATE TABLE `%s` (
+                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                  `time` datetime DEFAULT NULL,
+                  `open` double DEFAULT NULL,
+                  `high` double DEFAULT NULL,
+                  `low` double DEFAULT NULL,
+                  `close` double DEFAULT NULL,
+                  `volume` double DEFAULT NULL,
+                  PRIMARY KEY (`id`),
+                  KEY `ix_bfnbffh1_id` (`id`)
+                ) ENGINE=InnoDB AUTO_INCREMENT=43192 DEFAULT CHARSET=utf8;
+            """%(name)
+            conn.execute(create_statement)
+            df.to_sql(name, conn, index_label='id', if_exists='replace', chunksize=1000)
         else:
             Base.classes
             cl = Base.classes[name]
             latest_date = session.query(cl.time).order_by(cl.id.desc()).first()[0]
             new_df = df[df['time'] > latest_date]
-            new_df.to_sql(name, conn, index=False, if_exists='append')
+            new_df.to_sql(name, conn, index=False, if_exists='append', chunksize=1000)
+            del new_df
+        del df
 
 if __name__ == "__main__":
     fire.Fire(record_hst_data_to_mysql)
